afb02d39997d5e62c1b351cdf42b49e7aa4fa5b3
Bingsa <lincolnharrison8pc@gmail.com>
2026-03-17 16:33:55 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,17 @@
-const CACHE_NAME = 'cache-v2';
+const CACHE_NAME = 'cricket-cache-v1';
 const urlsToCache = [
+  './',
   './index.html',
-  './google-cricket/index.html',
-  './robots.txt',
-  './privacy-policy.html',
-  './assets/js/libs.min.js',
-  './assets/js/main.min.js',
-  './assets/css/main.min.css',
-  './icons/icon-72x72.png',
-  './icons/icon-96x96.png',
-  './icons/icon-128x128.png',
-  './icons/icon-144x144.png',
-  './icons/icon-152x152.png',
-  './icons/icon-192x192.png',
-  './icons/icon-384x384.png',
-  './icons/icon-512x512.png',
+  './sw.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => console.log('Cache successfully initialized'))
-      .catch(error => console.log('Cache initialization failed:', error))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -30,25 +19,11 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
       })
-      .catch(error => console.log('Fetch error:', error))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
